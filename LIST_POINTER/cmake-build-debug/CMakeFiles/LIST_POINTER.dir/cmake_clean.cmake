d821ed6760bb3adeaaf5c5f30723e3763871b6cb
Krym4s <70252345+Krym4s@users.noreply.github.com>
2020-11-08 19:00:35 +0300

    Add files via upload

---
+++ LIST_POINTER/cmake-build-debug/CMakeFiles/LIST_POINTER.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LIST_POINTER.dir/LIST_POINTER.cpp.obj"
+  "CMakeFiles/LIST_POINTER.dir/main.cpp.obj"
+  "LIST_POINTER.exe"
+  "LIST_POINTER.exe.manifest"
+  "LIST_POINTER.pdb"
+  "libLIST_POINTER.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LIST_POINTER.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
